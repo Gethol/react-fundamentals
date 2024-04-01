76c46232f0fe64b60dbc4f9d3c37e34c36da4ba0
Nathan Nderitu <natenderitu@gmail.com>
2024-04-01 15:12:00 +0300

    feat: context passes through intermediate components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import MyApp from './components/MyApp.tsx';
 import Page from './components/Page.tsx';
+import ProfilePage from './components/ProfilePage.jsx';
 
 function App() {
   return (
-    <Page />
+    // <Page />
     // {/* <MyApp /> */}
-    
+    <ProfilePage/>
     );
 }
 
