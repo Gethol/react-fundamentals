1a3e913483f455eb4f57301cb8a465e053ca3ed6
Nathan Nderitu <natenderitu@gmail.com>
2024-04-01 13:59:38 +0300

    feat: rendering lists

--- src/components/MyApp.tsx
+++ src/components/MyApp.tsx
@@ -7,6 +7,29 @@ const user = {
 
 }
 
+const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+]
+
+function ShoppingList(){
+    const listItems = products.map(product =>
+        <li
+        key={product.id}
+        style={{
+            color: product.isFruit ? 'magenta' : 'darkgreen'
+        }}
+        >
+            {product.title}
+        </li>
+        )
+
+        return (
+            <ul>{listItems}</ul>
+        )
+}
+
 function Profile(){
     return(
         <>
@@ -48,6 +71,7 @@ export default function MyApp(){
             <Button />
             <AboutPage/>
             <Profile />
+            <ShoppingList />
         </div>
     );
 }
