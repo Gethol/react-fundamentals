76ddac145c4b59174f5c5251741461462cdabf83
Nathan Nderitu <natenderitu@gmail.com>
2024-04-01 14:38:59 +0300

    feat: create the context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import MyApp from './components/MyApp.tsx';
+import Page from './components/Page.tsx';
 
 function App() {
   return (
-    <MyApp />
+    <Page />
+    // {/* <MyApp /> */}
+    
     );
 }
 
